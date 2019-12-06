19d8d1b5fb10a2035f979874b5946b98144458f1
Richard Darst <rkd@zgib.net>
2019-12-06 02:47:12 +0200

    update tests

--- test.sh
+++ test.sh
@@ -2,22 +2,67 @@
 set -x
 set -e
 
-chmod -R u+w ./tmp*
-rm -rf ./tmp*
-tmpdir=$(mktemp -d -p $(dirname $0))
-base="$PWD/$tmpdir"
+echo "$@"
 
+# ROOT is where test is done, path of this script by default,
+# otherwise from command line.
+ROOT=$(dirname $0)
+test -n $1 && ROOT=$1 || true
+ROOT=$(readlink -f $ROOT)
+
+# Remove existing test dirs
+chmod -R u+w $ROOT/tmp-git-data-test* || true
+rm -rf $ROOT/tmp-git-data-test*
+
+# Make temp dir, change to it
+tmpdir=$(mktemp -d -p $ROOT tmp-git-data-test-XXXXXXX)
 cd $tmpdir
-mkdir base
-cd base
-git init
+
+# Make first repo, init it, add a file.
+mkdir origin
+cd 'origin'
+git init 'test origin'
 git data init
 echo data1 >> file1
 git data add file1
+git commit -m 'commit 1'
 
-cd "$base"
+# Clone it, verify that "git data clone" automatically gets for you
+cd "$tmpdir"
 ls
-git data clone ./base ./clone1
+git data clone ./origin ./clone1
+cd 'clone1'
+test -e file1  # "git data clone" inits and gets for you.
+git data sync
+test -e file1
 
+# Clone it, verify that "git data clone" automatically gets for you
+cd "$tmpdir"
+ls
+git clone ./origin ./clone2
+cd 'clone2'
+test ! -e file1  # "git data clone" inits and gets for you.
+git data sync --content
+test -e file1
+
+
+echo
+echo "phase 1 success"
+echo
+
+# Phase 2: try to link to object store
+test -z "$2" && exit 0
+
+echo $2
+echo "$@"
+
+# Do the test
+cd "$tmpdir/origin"
+git data allas "$2"
+git data sync --content
+
+echo
+echo "phase 2 success"
+echo
 
 #trap "rm -r ./$tmpdir" EXIT
